2cd9c6c4864f469903a274bd97d324ed5daa7cbf
Ray Chidaz <raychidaz@gmail.com>
2020-05-12 17:23:30 +0100

    implemet login,review callback URL & JWT, parse callback URL in Callback component, picked a token storage approach(localStorage), implement HandleAuthentication and checed if user is authenticated

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -11,4 +11,36 @@ export default class Auth {
       scope: "openid profile email",
     });
   }
+  login = () => {
+    this.auth0.authorize();
+  };
+
+  handleAuthentication = () => {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+        this.history.push("/");
+      } else if (err) {
+        this.history.push("/");
+        alert(`Error: ${err.error}. Check the console for further details.`);
+        console.log(err);
+      }
+    });
+  };
+
+  setSession = (authResult) => {
+    // set the time that the access token will expire
+    const expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+
+    localStorage.setItem("access_token", authResult.accessToken);
+    localStorage.setItem("id_token", authResult.idToken);
+    localStorage.setItem("expires_at", expiresAt);
+  };
+
+  isAuthenticated() {
+    const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
+    return new Date().getTime() < expiresAt;
+  }
 }
